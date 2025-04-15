700e69e06ea3cf2090893d63023da9eaa7c014f4
megupta06 <103089880+megupta06@users.noreply.github.com>
2025-04-15 14:29:57 -0700

    Update lab4.js

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -26,12 +26,15 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (!Array.isArray(prices) || typeof discount !== 'number' || discount < 0 || discount > 1) {
+        return false;
+    }
     const discounted = []
     const length = prices.length;
     if (length === 0) return false;
-    let discountedPrice = 0
+    //let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice = prices[i] * (1 - discount);
+        let discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }  
     return discounted;
